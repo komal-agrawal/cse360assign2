963ae0aa1e437899b0cb44230078528b12da714d
komal-agrawal <35928647+komal-agrawal@users.noreply.github.com>
2019-02-24 13:59:48 -0700

    Create Calculator.java
    Initial commit

---
+++ Calculator.java
@@ -0,0 +1,54 @@
+/**
+ * Class ID: 303
+ * <p>
+ * Assignment Number: 2
+ * <p>
+ * File Description: File contains Calculator class with the purpose of 
+ * performing basic functions of a calculator. It includes a add method, 
+ * subtract method, getTotal method, and divide method. There is also a 
+ * getHistory method that returns the history of all actions as a String. 
+ * 
+ * @author Komal Agrawal
+ * @since 2019-02-24
+ */
+package cse360assign2;
+
+/**
+ * Performs basic computations such as addition, subtraction, and division from
+ * a parameter to the total. 
+ * The history of the computations are tracked and can be retrieved from the 
+ * getHistory method. 
+ *
+ */
+public class Calculator {
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+	
+	public void multiply (int value) {
+		
+	}
+	
+	public void divide (int value) {
+		
+	}
+	
+	public String getHistory () {
+		return "";
+	}
+
+}
